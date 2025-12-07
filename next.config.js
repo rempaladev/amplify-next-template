8c6ce3f4e473225783964989f081e8ff4d5755b4
rempaladev <rempaladev@gmail.com>
2025-12-07 11:38:03 -0600

    Add config for api variable

--- next.config.js
+++ next.config.js
@@ -3,6 +3,8 @@ const nextConfig = {
   env: {
     ELEVENLABS_AGENT_ID: process.env.ELEVENLABS_AGENT_ID,
     ELEVENLABS_API_KEY: process.env.ELEVENLABS_API_KEY,
+    OPENAI_API_KEY: process.env.OPENAI_API_KEY,
+
   }
 }
 
